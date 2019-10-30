9d4997c66f08fd0cf2350a772fec0780cf750813
Longtonry <52749757+Longtonry@users.noreply.github.com>
2019-10-30 12:01:59 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,6 +1,15 @@
 package com.example;
+/**
+* .
+*/
 public class Greeter {
+  /**
+  * .
+  */
   public Greeter() { }
+  /**
+  * .
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
